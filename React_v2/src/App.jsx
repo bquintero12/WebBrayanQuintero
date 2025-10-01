a7cd7a72ba6aa0de281e21294b7bb25ede3600f5
bquintero12 <quinterosteven26@gmail.com>
2025-10-01 13:01:26 -0500

    Al dia con React V2

---
+++ React_v2/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Header from './Componets/Header/Header'
+import Footer from './Componets/Footer/Footer'
+import CardSection from './Componets/CardSection/CardSection'
+
+function App() {
+  
+  return (
+    <>
+      <Header />      
+      <main>
+        <CardSection />      
+        <CardSection />      
+        
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ React_v2/src/Componets/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>2025. Hecho con Amor por S:P.C.</p>
+            <p>Todos los derechos reservados</p>
+        </footer>
+    );
+};
+
+export default Footer;
